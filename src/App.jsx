29c555db87ac70255c90c5a89f842adc193e9fc6
Guilherme Gomes <guilhermegg018@gmail.com>
2023-05-21 22:07:46 -0300

    tentando publicar o site

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { ThemeProvider } from "styled-components"
 import "./App.css"
-import { Home } from "./pages/Home"
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 import { Routers } from "./routes"
