9e6d2911a7b6cf0248d7f383b97cd3af19aa34f3
Guilherme Gomes <guilhermegg018@gmail.com>
2023-05-22 00:53:51 -0300

    add changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/estagio-itegra/",
+  base: "/estagio-itegra",
   plugins: [react()],
 })
